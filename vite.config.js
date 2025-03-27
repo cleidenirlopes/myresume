8cc6d9ff9a2865ae397e835c3032e88f42e4b9f3
Cledenir de Souza <cleidenirlopes@outlook.com>
2025-03-27 19:54:44 +0000

    Simplify build configuration

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    sourcemap: true
   }
 })
