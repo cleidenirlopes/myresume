9acae69ec5d2d1ddcb42fc9acc97f7d94429d988
Cledenir de Souza <cleidenirlopes@outlook.com>
2025-03-29 11:40:45 +0000

    Version03

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/myresume/', // Set this to the subpath where your app is deployed
   plugins: [react()],
-})
+});
