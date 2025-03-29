0b3be946b878bea52a6b6e2c7aa28bcb5c1196b2
Cledenir de Souza <cleidenirlopes@outlook.com>
2025-03-29 11:25:49 +0000

    Version

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/myresume/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  }
 })
