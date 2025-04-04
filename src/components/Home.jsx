dcfd0e1de18ab46276af773afdaee844939fee7b
Cledenir de Souza <139762768+cleidenirlopes@users.noreply.github.com>
2025-04-04 09:32:13 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,26 +2,24 @@ import React from 'react';
 
 const Home = () => {
   return (
-               <div className="min-h-screen flex-[3] transition-all duration-500 bg-[#ffffff] text-[#151a1e] dark:bg-[#151a1e] dark:text-[#ffffff] font-light">
-        {/* Profile Section */}
-        <div className="flex flex-col items-center justify-center gap-5 relative mt-20 mr-20 max-w-3xl">
-          {/* Added margin-top to create space above */}
-          <div className="space-y-2 text-center">
-            <h1 className="text-6xl font-extralight tracking-wide">
-              <span className="text-black dark:text-white">
-                Cledenir
-              </span> 
-              <span className="font-medium text-[#2b88c6]">
-                Souza
-              </span>
-            </h1>
-            <h3 className="text-3xl font-extralight tracking-wider text-[#151a1e] dark:text-[#d8dcde]">
-              Data Analyst
-            </h3>
-          </div>
+    <div className="min-h-screen flex-[3] transition-all duration-500 bg-[#ffffff] text-[#151a1e] dark:bg-[#151a1e] dark:text-[#ffffff] font-light">
+      {/* Profile Section */}
+      <div className="flex flex-col items-center justify-center gap-5 relative mt-20 mr-20 max-w-3xl">
+        {/* Added margin-top to create space above */}
+        <div className="space-y-2 text-center">
+          <h1 className="text-6xl font-extralight tracking-wide">
+            <span className="text-black dark:text-white">
+              Cledenir
+            </span>
+            <span className="font-medium text-[#2b88c6]">
+              Souza
+            </span>
+          </h1>
+          <h3 className="text-3xl font-extralight tracking-wider text-[#151a1e] dark:text-[#d8dcde]">
+            Data Analyst
+          </h3>
         </div>
       </div>
-
 
       {/* Main Section */}
       <main className="flex max-w-[1000px] flex-col gap-10 p-5 md:py-14 mr-20">
@@ -43,75 +41,63 @@ const Home = () => {
 
         {/* Job Experience Section */}
         <div className="flex flex-col items-start justify-start gap-5 border-b border-gray-300 dark:border-gray-700 p-5 max-w-3xl">
-            <ul className="relative">
-              {/* Vertical Line */}
-              <div className="absolute left-[9px] top-0 h-full w-[1px] bg-gray-300 dark:bg-gray-700"></div>
+          <ul className="relative">
+            {/* Vertical Line */}
+            <div className="absolute left-[9px] top-0 h-full w-[1px] bg-gray-300 dark:bg-gray-700"></div>
 
-              {/* Entain Group */}
-              <li className="relative flex flex-col gap-1 pl-8">
-                <div
-                  className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full 
-                bg-[#2b88c6]"
-                ></div>
-                 <div className="flex justify-between items-center w-full gap-32">
-                  <span className="font-medium tracking-wide md:text-lg">CUSTOMER SUPPORT ANALYST</span>
-                  <span className="text-sm text-gray-800 font-light">2024 - Current</span>
-                </div>
-                <span className="font-light tracking-wide mb-4">Entain Group ¦ Lisbon</span>
-                <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base font-light leading-relaxed">
-                  Demonstrated expertise in providing exceptional customer support within a fast-paced global gaming environment. Successfully managed complex technical inquiries while maintaining a 95% customer satisfaction rate. Leveraged analytical skills to identify patterns in customer issues, contributing to process improvements and enhanced user experience. Collaborated with international teams to streamline support workflows and implement data-driven solutions for recurring challenges.
-                </p>
-              </li>
+            {/* Entain Group */}
+            <li className="relative flex flex-col gap-1 pl-8">
+              <div className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full bg-[#2b88c6]"></div>
+              <div className="flex justify-between items-center w-full gap-32">
+                <span className="font-medium tracking-wide md:text-lg">CUSTOMER SUPPORT ANALYST</span>
+                <span className="text-sm text-gray-800 font-light">2024 - Current</span>
+              </div>
+              <span className="font-light tracking-wide mb-4">Entain Group ¦ Lisbon</span>
+              <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base font-light leading-relaxed">
+                Demonstrated expertise in providing exceptional customer support within a fast-paced global gaming environment. Successfully managed complex technical inquiries while maintaining a 95% customer satisfaction rate. Leveraged analytical skills to identify patterns in customer issues, contributing to process improvements and enhanced user experience. Collaborated with international teams to streamline support workflows and implement data-driven solutions for recurring challenges.
+              </p>
+            </li>
 
-              {/* Accenture */}
-              <li className="relative flex flex-col gap-1 pl-8 mt-6">
-                <div
-                  className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full 
-                bg-[#2b88c6]"
-                ></div>
-                <div className="flex justify-between items-center w-full gap-32">
-                  <span className="font-medium tracking-wide md:text-lg">CONTENT MODERATION SPECIALIST</span>
-                  <span className="text-sm text-gray-800 font-light">2023 - 2024</span>
-                </div>
-                <span className="font-light tracking-wide mb-4">Accenture ¦ Lisbon</span>
-                <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base font-light leading-relaxed">
-                  Managed content moderation operations using data-driven approaches to maintain platform integrity. Utilized advanced CRM tools to track and analyze user interactions, creating detailed reports that led to a 30% improvement in response efficiency. Implemented systematic data collection methods to identify trending issues and patterns, contributing to preventive measures. Collaborated with analytics teams to develop KPI dashboards for monitoring moderation effectiveness and team performance metrics.
-                </p>
-              </li>
+            {/* Accenture */}
+            <li className="relative flex flex-col gap-1 pl-8 mt-6">
+              <div className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full bg-[#2b88c6]"></div>
+              <div className="flex justify-between items-center w-full gap-32">
+                <span className="font-medium tracking-wide md:text-lg">CONTENT MODERATION SPECIALIST</span>
+                <span className="text-sm text-gray-800 font-light">2023 - 2024</span>
+              </div>
+              <span className="font-light tracking-wide mb-4">Accenture ¦ Lisbon</span>
+              <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base font-light leading-relaxed">
+                Managed content moderation operations using data-driven approaches to maintain platform integrity. Utilized advanced CRM tools to track and analyze user interactions, creating detailed reports that led to a 30% improvement in response efficiency. Implemented systematic data collection methods to identify trending issues and patterns, contributing to preventive measures. Collaborated with analytics teams to develop KPI dashboards for monitoring moderation effectiveness and team performance metrics.
+              </p>
+            </li>
 
-              {/* Wisdom */}
-              <li className="relative flex flex-col gap-1 pl-8 mt-6">
-                <div
-                  className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full 
-                bg-[#2b88c6]"
-                ></div>
-                 <div className="flex justify-between items-center w-full gap-32">
-                  <span className="font-medium tracking-wide md:text-lg">DESKTOP SUPPORT TECHNICIAN</span>
-                  <span className="text-sm text-gray-800 font-light">2014 - 2017</span>
-                </div>
-                <span className="font-light tracking-wide mb-4">Wisdom Hardware ¦ São Paulo</span>
-                <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base font-light leading-relaxed">
-                  Technical support operations for a major hardware retailer, managing system diagnostics and performance optimization. Developed and maintained detailed technical documentation, reducing average resolution time by 25%. Implemented systematic tracking of hardware failures and software issues, enabling data-driven inventory and maintenance decisions. Coordinated with vendors to analyze performance metrics and optimize hardware lifecycle management.
-                </p>
-              </li>
+            {/* Wisdom */}
+            <li className="relative flex flex-col gap-1 pl-8 mt-6">
+              <div className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full bg-[#2b88c6]"></div>
+              <div className="flex justify-between items-center w-full gap-32">
+                <span className="font-medium tracking-wide md:text-lg">DESKTOP SUPPORT TECHNICIAN</span>
+                <span className="text-sm text-gray-800 font-light">2014 - 2017</span>
+              </div>
+              <span className="font-light tracking-wide mb-4">Wisdom Hardware ¦ São Paulo</span>
+              <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base font-light leading-relaxed">
+                Technical support operations for a major hardware retailer, managing system diagnostics and performance optimization. Developed and maintained detailed technical documentation, reducing average resolution time by 25%. Implemented systematic tracking of hardware failures and software issues, enabling data-driven inventory and maintenance decisions. Coordinated with vendors to analyze performance metrics and optimize hardware lifecycle management.
+              </p>
+            </li>
 
-              {/* Souza Cruz */}
-              <li className="relative flex flex-col gap-1 pl-8 mt-6">
-                <div
-                  className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full 
-                bg-[#2b88c6]"
-                ></div>
-                <div className="flex justify-between items-center w-full gap-32">
-                  <span className="font-medium tracking-wide md:text-lg">IT SUPPORT SPECIALIST</span>
-                  <span className="text-sm text-gray-800 font-light">2005 - 2011</span>
-                </div>
-                <span className="font-light tracking-wide mb-4">Souza Cruz ¦ São Paulo</span>
-                <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base font-light leading-relaxed">
-                  Provided comprehensive IT support for regional office operations, focusing on hardware troubleshooting and network maintenance. Developed and maintained internal documentation systems to track recurring technical issues and solutions. Created monthly reports to analyze support tickets and system performance, helping identify areas for improvement. Collaborated with the local IT team to implement preventive maintenance procedures and optimize system efficiency.
-                </p>
-              </li>
-            </ul>
-          </div>
+            {/* Souza Cruz */}
+            <li className="relative flex flex-col gap-1 pl-8 mt-6">
+              <div className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full bg-[#2b88c6]"></div>
+              <div className="flex justify-between items-center w-full gap-32">
+                <span className="font-medium tracking-wide md:text-lg">IT SUPPORT SPECIALIST</span>
+                <span className="text-sm text-gray-800 font-light">2005 - 2011</span>
+              </div>
+              <span className="font-light tracking-wide mb-4">Souza Cruz ¦ São Paulo</span>
+              <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base font-light leading-relaxed">
+                Provided comprehensive IT support for regional office operations, focusing on hardware troubleshooting and network maintenance. Developed and maintained internal documentation systems to track recurring technical issues and solutions. Created monthly reports to analyze support tickets and system performance, helping identify areas for improvement. Collaborated with the local IT team to implement preventive maintenance procedures and optimize system efficiency.
+              </p>
+            </li>
+          </ul>
+        </div>
       </main>
 
       {/* Courses Section */}
@@ -121,15 +107,14 @@ const Home = () => {
           <div className="relative max-w-3xl mt-6">
             {/* Left vertical line */}
             <div className="absolute left-[9px] top-[1px] h-[calc(100%-8px)] w-[1px] bg-gray-300 dark:bg-gray-700"></div>
-            
+
             {/* Right vertical line */}
             <div className="absolute md:left-[calc(50%+25px)] left-[9px] top-[1px] h-[calc(100%-8px)] w-[1px] bg-gray-300 dark:bg-gray-700"></div>
-            
+
             <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-6">
               {/* Course 1 */}
               <div className="relative pl-8">
-                <div  className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full 
-                bg-[#2b88c6]"></div>
+                <div className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full bg-[#2b88c6]"></div>
                 <div className="space-y-1">
                   <h3 className="font-light tracking-wide text-base">Data Visualization: A Lesson and Listen Series</h3>
                   <p className="font-light text-base text-gray-600 dark:text-gray-400">Linkedin Learning</p>
@@ -139,36 +124,15 @@ const Home = () => {
 
               {/* Course 2 */}
               <div className="relative pl-8">
-                <div  className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full 
-                bg-[#2b88c6]"></div>
+                <div className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full bg-[#2b88c6]"></div>
                 <div className="space-y-1">
-                  <h3 className="font-light tracking-wide text-base">Advanced Python: Practical Database Examples</h3>
-                  <p className="font-light text-base text-gray-600 dark:text-gray-400">Linkeding Learning</p>
-                  <p className="text-sm text-gray-800">2025</p>
-                </div>
-              </div>
-
-              {/* Course 3 */}
-              <div className="relative pl-8">
-                <div  className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full 
-                bg-[#2b88c6]"></div>
-                <div className="space-y-1">
-                  <h3 className="font-light tracking-wide text-base">Financial Modeling Foundations</h3>
-                  <p className="font-light text-base text-gray-600 dark:text-gray-400">Linkedin Learning</p>
-                  <p className="text-sm text-gray-800">2023</p>
-                </div>
-              </div>
-
-              {/* Course 4 */}
-              <div className="relative pl-8">
-                <div  className="absolute left-[4px] top-[8px] h-3 w-3 rounded-full 
-                bg-[#2b88c6]"></div>
-                <div className="space-y-1">
-                  <h3 className="font-light tracking-wide text-base">Google Data Analytics Professional Certificate</h3>
+                  <h3 className="font-light tracking-wide text-base">SQL for Data Science</h3>
                   <p className="font-light text-base text-gray-600 dark:text-gray-400">Coursera</p>
-                  <p className="text-sm text-gray-800">2023</p>
+                  <p className="text-sm text-gray-800">2024</p>
                 </div>
               </div>
+
+              {/* Add more courses as needed */}
             </div>
           </div>
         </div>
@@ -178,5 +142,3 @@ const Home = () => {
 }
 
 export default Home;
-
-
